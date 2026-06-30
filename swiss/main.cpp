f80c1e3d5f35f13edcd1a3ef7ee522ee90169c23
Cheese-S <cheese_s@hotmail.com>
2026-06-29 19:10:41 -0700

    build

--- swiss/main.cpp
+++ swiss/main.cpp
@@ -1,6 +1,28 @@
-#include <iostream>
+#include <swiss/PathTracer.h>
+#include <core/log/IRawLog.h>
+#include <core/IMemory.h>
+
+namespace mk::swiss
+{
+namespace
+{
+bool run()
+{
+    UniquePtr<PathTracer> pathTracer;
+    Result                res = PathTracer::makePathTracer(pathTracer);
+    if (!isOk(res))
+    {
+        return false;
+    }
+    return isOk(pathTracer->run());
+}
+} // namespace
+
+} // namespace mk::swiss
 
 int main()
 {
-    std::cout << "Hello world";
+    mk::mm::details::forceMiMallocLinkOrder();
+    mk::swiss::run();
+    return 0;
 }
